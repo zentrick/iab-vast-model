715a873a537c807e95c968d52298413de175f3a3
Tim De Pauw <hi@tmdpw.eu>
2016-07-17 07:50:23 +0200

    Improve gulpfile

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,20 +4,28 @@ import {Instrumenter} from 'isparta'
 import del from 'del'
 import seq from 'run-sequence'
 import spawn from 'cross-spawn-promise'
+import yargs from 'yargs'
 
 const COVERAGE_THRESHOLDS = {global: 100}
+const {COVERALLS} = process.env
 
 const $ = loadPlugins()
+const argv = yargs
+  .string('grep')
+  .boolean('bail')
+  .argv
 
-const plumb = () => $.if(!process.env.CI, $.plumber({
-  errorHandler: $.notify.onError('<%= error.message %>')
-}))
+const unitTest = () => gulp.src(['test/lib/setup.js', 'test/unit/**/*.js'], {read: false})
+  .pipe($.mocha({
+    reporter: 'spec',
+    grep: argv.grep,
+    bail: argv.bail
+  }))
 
 gulp.task('clean', () => del('lib'))
 
-gulp.task('transpile', () => {
+gulp.task('build', ['clean'], () => {
   return gulp.src('src/**/*.js')
-    .pipe(plumb())
     .pipe($.sourcemaps.init())
     .pipe($.babel())
     .pipe($.sourcemaps.write())
@@ -26,27 +34,30 @@ gulp.task('transpile', () => {
 
 gulp.task('lint', () => {
   return gulp.src('{src,test}/**/*.js')
-    .pipe(plumb())
     .pipe($.standard())
-    .pipe($.standard.reporter('default', {breakOnError: false}))
+    .pipe($.standard.reporter('default', {
+      breakOnError: false
+    }))
 })
 
-gulp.task('pre-coverage', () => {
+gulp.task('test:unit', unitTest)
+
+gulp.task('coverage:instrument', () => {
   return gulp.src('src/**/*.js')
-    .pipe($.istanbul({instrumenter: Instrumenter}))
+    .pipe($.istanbul({
+      instrumenter: Instrumenter
+    }))
     .pipe($.istanbul.hookRequire())
 })
 
-gulp.task('coverage', ['pre-coverage'], () => {
-  return gulp.src(['test/lib/setup.js', 'test/{unit,integration}/**/*.js', '!**/_*.js'], {read: false})
-    .pipe(plumb())
-    .pipe($.mocha({reporter: 'spec'}))
+gulp.task('coverage', ['coverage:instrument'], () => {
+  return unitTest()
     .pipe($.istanbul.writeReports())
     .pipe($.istanbul.enforceThresholds({thresholds: COVERAGE_THRESHOLDS}))
 })
 
 gulp.task('coveralls', () => {
-  if (!process.env.COVERALLS) {
+  if (!COVERALLS) {
     return
   }
   return gulp.src('coverage/lcov.info')
@@ -55,16 +66,15 @@ gulp.task('coveralls', () => {
 
 gulp.task('test', (cb) => seq('lint', 'coverage', 'coveralls', cb))
 
-gulp.task('build', (cb) => seq('test', 'clean', 'transpile', cb))
-
 gulp.task('watch', () => gulp.watch('{src,test}/**/*', ['build']))
 
-gulp.task('clean-doc', () => del('doc'))
+gulp.task('doc:clean', () => del('doc'))
 
-gulp.task('doc', ['clean-doc'], () => {
+gulp.task('doc', ['doc:clean'], () => {
   const cmd = 'node_modules/.bin/jsdoc'
   const args = ['-c', 'jsdoc.json']
-  return spawn(cmd, args, {cwd: __dirname, stdio: 'inherit'})
+  const options = {cwd: __dirname, stdio: 'inherit'}
+  return spawn(cmd, args, options)
 })
 
 gulp.task('default', ['build'], () => gulp.start('watch'))
